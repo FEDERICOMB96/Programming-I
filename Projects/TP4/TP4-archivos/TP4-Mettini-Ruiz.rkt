ecddab5e834c59834c1b15c572a6e0b8786af159
Fabrizio Mettini <mettinif@gmail.com>
2020-06-16 11:08:44 -0300

    Change README, Finish Project 4

--- Projects/TP4/TP4-archivos/TP4-Mettini-Ruiz.rkt
+++ Projects/TP4/TP4-archivos/TP4-Mettini-Ruiz.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname TP4-Mettini-Ruiz) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname TP4-Mettini-Ruiz) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
 #|
 Trabajo Práctico 4: Listas
 
@@ -29,7 +29,6 @@ Integrantes:
 ; - sosp casos estaban en estudio.
 ; El último elemento, notif, indica la cantidad total de notificaciones.
 
-
 ;;;;;;;;;;;; Preparación de los Datos
 
 ;;;;;; Datos sobre localidades santafesinas
@@ -42,8 +41,10 @@ Integrantes:
 ; Dada una lista l de dos o más elementos, tomar-dos calcula
 ; la lista formada por los dos primeros elementos de l, en
 ; ese orden.
+
 (check-expect (tomar-dos (list "a" "b")) (list "a" "b")) 
 (check-expect (tomar-dos (list 0 1 2 3)) (list 0 1))
+
 (define
   (tomar-dos l)
   (list (first l) (second l)))
@@ -60,7 +61,24 @@ Integrantes:
 (define DATOS-NOTIF (rest INPUT-NOTIF))
 
 ; Lista de notificaciones
+;; Ejercicio 1
+;; list->notificacion : List -> Notificacion
+;; Dada una lista, devuelve un elemento de tipo notificacion.
 
+(define LISTA-ESPERANZA-2020-06-02 (list "2020-06-02" "ESPERANZA" "1" "63" "0" "64"))
+(define NOTIF-ESPERANZA-2020-06-02 (make-notificacion "2020-06-02" "ESPERANZA" 1 63 0 64))
+
+(check-expect (list->notificacion LISTA-ESPERANZA-2020-06-02) NOTIF-ESPERANZA-2020-06-02)
+
+(define (list->notificacion l)
+  (make-notificacion (first l)
+                     (second l)
+                     (string->number (third l))
+                     (string->number (fourth l))
+                     (string->number (fifth l))
+                     (string->number (sixth l))))
+
+(define LISTA-NOTIF (map list->notificacion DATOS-NOTIF))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Consultas ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,20 +90,228 @@ Integrantes:
 
 ;;;;;;;;;;;; Consulta 1
 
-; [Completar, ejercicio 2-1]
+;; Ejercicio 2-1
+;; Algunos Ejemplos de Notificaciones
+(define NOTIF-ROSARIO-2020-06-02 (make-notificacion "2020-06-02" "ROSARIO" 113 5053 72 5238))
+(define NOTIF-TOSTADO-2020-06-02 (make-notificacion "2020-06-02" "TOSTADO" 0 6 0 6))
+(define NOTIF-RAFAELA-2020-06-01 (make-notificacion "2020-06-01" "RAFAELA" 21 193 8 222))
 
-; [Completar, ejercicio 2-2]
+;; notificacion-actual? : Notificacion -> Boolean
+;; Dada una notificacion, devuelve si dicha notificacion es una notificacion del dia HOY.
+
+(check-expect (notificacion-actual? NOTIF-ROSARIO-2020-06-02) #t)
+(check-expect (notificacion-actual? NOTIF-RAFAELA-2020-06-01) #f)
+
+(define (notificacion-actual? n)
+  (string=? (notificacion-fecha n) HOY))
+
+;; mayor-o-igual-LIMITE-CASOS? : Notificacion -> Boolean
+;; Dada una notificacion, devuelve si dicha notificacion indica que la localidad
+;; presenta una cantidad mayor o igual de casos confirmados que LIMITE-CASOS.
+
+(check-expect (mayor-o-igual-LIMITE-CASOS? NOTIF-ROSARIO-2020-06-02) #t)
+(check-expect (mayor-o-igual-LIMITE-CASOS? NOTIF-TOSTADO-2020-06-02) #f)
+
+(define (mayor-o-igual-LIMITE-CASOS? n)
+  (>= (notificacion-conf n) LIMITE-CASOS))
+
+;; localidades-limite-casos : List(Notificacion) -> List(String)
+;; Dada una lista de notificaciones, devuelva la lista de localidades que presentan
+;; LIMITE-CASOS o más casos confirmados de COVID-19 hasta el día HOY.
+
+(check-expect (localidades-limite-casos empty) empty)
+(check-expect (localidades-limite-casos LISTA-NOTIF) (list "ROSARIO" "SANTA FE"))
+
+(define (localidades-limite-casos l)
+  (map notificacion-loc                           ; Obtencion de los nombres de la localidades
+       (filter mayor-o-igual-LIMITE-CASOS?        ; Filtrar notificaciones por su cantidad de casos confirmados
+               (filter notificacion-actual? l)))) ; Filtrar notificaciones por su fecha
+
+;; Ejercicio 2-2
+(define LOCALIDADES-LIMITE-CASOS (localidades-limite-casos LISTA-NOTIF))
 
 ;;;;;;;;;;;; Consulta 2
 
-; [Completar, ejercicio 3-1]
+;; Ejercicio 3-1
+;; esta? : String List(String) -> Boolean
+;; Dado un String s y una lista de strings l, devuelve si s esta en l.
 
-; [Completar, ejercicio 3-2]
+(check-expect (esta? "a" empty) #f)
+(check-expect (esta? "a" (list "a" "b" "b" "a" "c")) #t)
 
-; [Completar, ejercicio 3-3]
+(define (esta? s l)
+  (cond [(empty? l) #f]
+        [else (if (string=? s (first l))
+                  #t
+                  (esta? s (rest l)))]))
 
-; [Completar, ejercicio 3-4]
+;; filtrar-strings-repetidas : List(String) -> List(String)
+;; Dada una lista con elementos de tipo String, devuelve una lista
+;; sin los elementos repetidos.
 
+(check-expect (filtrar-strings-repetidas empty) empty)
+(check-expect (filtrar-strings-repetidas (list "a" "b" "b" "a" "c")) (list "b" "a" "c"))
+
+(define (filtrar-strings-repetidas l)
+  (cond [(empty? l) empty]
+        [else (if (esta? (first l) (rest l))
+                  (filtrar-strings-repetidas (rest l))
+                  (cons (first l) (filtrar-strings-repetidas (rest l))))]))
+
+;; obtener-primero-sublistas : List(List(X ..)) -> List(X)
+;; Dada una lista de listas de elementos de cualquier tipo, devuelve una lista
+;; con el primer elemento de cada sublista.
+
+(check-expect (obtener-primero-sublistas empty) empty)
+(check-expect (obtener-primero-sublistas (list (list "a" "b")
+                                               (list "b" "c" "d")
+                                               (list "c")))
+              (list "a" "b" "c"))
+
+(define (obtener-primero-sublistas l)
+  (cond [(empty? l) empty]
+        [else (cons (first (first l)) (obtener-primero-sublistas (rest l)))]))
+
+(define LISTA-DPTO (filtrar-strings-repetidas (obtener-primero-sublistas LISTA-DPTO-LOC)))
+
+;; Ejercicio 3-2
+;; Otros Ejemplos de Notificaciones
+(define NOTIF-SUNCHALES-2020-05-08 (make-notificacion "2020-05-08" "SUNCHALES" 0 5 0 5))
+(define NOTIF-SANTA-FE-2020-06-02 (make-notificacion "2020-06-02" "SANTA FE" 27 855 45 927))
+(define NOTIF-SANTO-TOME-2020-06-02 (make-notificacion "2020-06-02" "SANTO TOME" 5 115 0 120))
+(define NOTIF-LAGUNA-PAIVA-2020-06-02 (make-notificacion "2020-06-02" "LAGUNA PAIVA" 0 8 1 9))
+
+;; Ejemplo de Lista de notificaciones
+(define LISTA-NOTIF-EJEMPLO
+  (list NOTIF-SUNCHALES-2020-05-08
+        NOTIF-RAFAELA-2020-06-01
+        NOTIF-ROSARIO-2020-06-02
+        NOTIF-SANTA-FE-2020-06-02
+        NOTIF-SANTO-TOME-2020-06-02
+        NOTIF-LAGUNA-PAIVA-2020-06-02))
+
+;; notificacion-del-dia-dado? : Notificacion String -> Boolean
+;; Dada una notificacion y una fecha, devuelve si la notificacion es de la fecha dada.
+
+(check-expect (notificacion-del-dia-dado? NOTIF-SUNCHALES-2020-05-08 HOY) #f)
+(check-expect (notificacion-del-dia-dado? NOTIF-SANTO-TOME-2020-06-02 HOY) #t)
+
+(define (notificacion-del-dia-dado? n f)
+  (string=? (notificacion-fecha n) f))
+
+;; filtrar-segun-fecha : List(Notificacion) String -> List(Notificacion)
+;; Dada una lista de notificaciones y una fecha, devuelve una lista de notificaciones
+;; de la fecha dada.
+
+(check-expect (filtrar-segun-fecha empty HOY) empty)
+(check-expect (filtrar-segun-fecha LISTA-NOTIF-EJEMPLO HOY)
+              (list NOTIF-ROSARIO-2020-06-02
+                    NOTIF-SANTA-FE-2020-06-02
+                    NOTIF-SANTO-TOME-2020-06-02
+                    NOTIF-LAGUNA-PAIVA-2020-06-02))
+
+(define (filtrar-segun-fecha l f)
+  (cond [(empty? l) empty]
+        [else (if (notificacion-del-dia-dado? (first l) f)
+                  (cons (first l) (filtrar-segun-fecha (rest l) f))
+                  (filtrar-segun-fecha (rest l) f))]))
+
+;; departamento : String List(String String) -> String
+;; Dada una localidad y una lista de listas con localidades y su departamento
+;; correspondiente, devuelve el departamento al que pertenece.
+
+(check-expect (departamento "CIUDAD INEXISTENTE" LISTA-DPTO-LOC) "")
+(check-expect (departamento "SANTA FE" LISTA-DPTO-LOC) "La Capital")
+
+(define (departamento loc l)
+  (cond [(empty? l) ""]
+        [else (if (string=? loc (second (first l)))
+                  (first (first l))
+                  (departamento loc (rest l)))]))
+
+;; notificacion-del-dpto-dado? : Notificacion String -> Boolean
+;; Dada una notificacion y un departamento, devuelve si la notificacion es de una
+;; localidad perteneciente al departamento dado.
+
+(check-expect (filtrar-segun-dpto NOTIF-SANTA-FE-2020-06-02 "La Capital") #t)
+(check-expect (filtrar-segun-dpto NOTIF-ROSARIO-2020-06-02 "La Capital") #f)
+
+(define (notificacion-del-dpto-dado? n d)
+  (string=? (departamento (notificacion-loc n) LISTA-DPTO-LOC) d))
+
+;; filtrar-segun-dpto : List(Notificacion) String -> List(Notificacion)
+;; Dada una lista de notificaciones y un departamento, devuelve una lista de notificaciones
+;; del departamento dado.
+
+(check-expect (filtrar-segun-dpto empty "La Capital") empty)
+(check-expect (filtrar-segun-dpto LISTA-NOTIF-EJEMPLO "La Capital")
+              (list NOTIF-SANTA-FE-2020-06-02
+                    NOTIF-SANTO-TOME-2020-06-02
+                    NOTIF-LAGUNA-PAIVA-2020-06-02))
+
+(define (filtrar-segun-dpto l d)
+  (cond [(empty? l) empty]
+        [else (if (notificacion-del-dpto-dado? (first l) d)
+                  (cons (first l) (filtrar-segun-dpto (rest l) d))
+                  (filtrar-segun-dpto (rest l) d))]))
+
+;; sumar-casos-confirmados : List(Notificacion) -> Number
+;; Dada una lista de notificaciones, devuelve la suma de todos los casos confirmados.
+
+(check-expect (sumar-casos-confirmados empty) 0)
+(check-expect (sumar-casos-confirmados LISTA-NOTIF-EJEMPLO) 166)
+
+(define (sumar-casos-confirmados l)
+  (cond [(empty? l) 0]
+        [else (+ (notificacion-conf (first l))
+                 (sumar-casos-confirmados (rest l)))]))
+
+;; confirmados-dpto-fecha : List(Notificacion) String String -> Number
+;; Dada una lista de notificaciones, un departamento y una fecha, calcula la cantidad de casos
+;; confirmados registrados en dicho departamento hasta la fecha en cuestión.
+
+(check-expect (confirmados-dpto-fecha empty "Castellanos" ANTES) 0)
+(check-expect (confirmados-dpto-fecha LISTA-NOTIF "Garay" HOY) 3)
+
+(define (confirmados-dpto-fecha l d f)
+  (sumar-casos-confirmados (filtrar-segun-dpto (filtrar-segun-fecha l f) d)))
+
+;; Ejercicio 3-3
+;; confirmados-dpto-fecha-2 : List(Notificacion) String String -> List(String Number)
+;; Dada una lista de notificaciones, un departamento y una fecha, devuelve una lista
+;; donde el primer elemento es el nombre del departamento y el segundo el número de casos
+;; confirmados que se han registrado allí hasta la fecha en cuestión.
+
+(check-expect (confirmados-dpto-fecha-2 empty "Castellanos" ANTES) empty)
+(check-expect (confirmados-dpto-fecha-2 LISTA-NOTIF "Garay" HOY) (list "Garay" 3))
+
+(define (confirmados-dpto-fecha-2 l d f)
+  (cond [(empty? l) empty]
+        [else (list d (confirmados-dpto-fecha l d f))]))
+
+;; confirmados-por-dpto-aux : List(Notificacion) List(String) String -> List(List(String Number))
+;; Dada una lista de notificaciones, una lista de departamentos y una fecha, devuelve una lista
+;; de listas de longitud dos: una por cada departamento santafesino, donde el primer elemento
+;; sea el nombre del departamento y el segundo sea el número de casos confirmados que se
+;; han registrado allí hasta la fecha en cuestión.
+
+(define (confirmados-por-dpto-aux l1 l2 f)
+  (cond [(empty? l2) empty]
+        [else (cons (confirmados-dpto-fecha-2 l1 (first l2) f)
+                    (confirmados-por-dpto-aux l1 (rest l2) f))]))
+
+;; confirmados-por-dpto : List(Notificacion) String -> List(List(String Number))
+;; Dada una lista de notificaciones y una fecha, devuelva una lista de listas de longitud dos:
+;; una por cada departamento santafesino, donde el primer elemento sea el nombre
+;; del departamento y el segundo sea el número de casos confirmados que se
+;; han registrado allí hasta la fecha en cuestión.
+
+(define (confirmados-por-dpto l f)
+  (confirmados-por-dpto-aux l LISTA-DPTO f))
+
+;; Ejercicio 3-4
+(define CONFIRMADOS-DPTO-ANTES (confirmados-por-dpto LISTA-NOTIF ANTES))
+(define CONFIRMADOS-DPTO-HOY (confirmados-por-dpto LISTA-NOTIF HOY))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Salidas ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -93,8 +319,38 @@ Integrantes:
 
 ;;;;;;;;;;;; Consulta 1
 
-; [Completar, ejercicio 4 - loc-lim-casos.csv]
+;; separar-filas : String -> String
+;; Dado un String s, devuelve otro es String que surge de añadir "\n " al final de s.
+
+(check-expect (separar-filas "") "\n ")
+(check-expect (separar-filas "ROSARIO") "ROSARIO\n ")
+
+(define (separar-filas s)
+  (string-append s "\n "))
+
+;; separar-columnas : List(String Number) -> String
+
+(check-expect (separar-columnas empty) "")
+(check-expect (separar-columnas (list "La Capital" 47)) "La Capital, 47")
+
+(define (separar-columnas l)
+  (cond [(empty? l) ""]
+        [else (string-append (first l) ", " (number->string (second l)))]))
+
+;; Ejercicio 4 - loc-lim-casos.csv
+(write-file "loc-lim-casos.csv"
+            (string-append "Localidades que presentan Limite de Casos\n "
+                           (foldr string-append "" (map separar-filas LOCALIDADES-LIMITE-CASOS))))
 
 ;;;;;;; Consulta 2
 
-; [Completar, ejercicio 4 - casos-por-dpto-hoy.csv y casos-por-dpto-antes.csv]
+;; Ejercicio 4 - casos-por-dpto-hoy.csv y casos-por-dpto-antes.csv
+(write-file "casos-por-dpto-hoy.csv"
+            (string-append "Departamento, Casos Confirmados\n "
+                           (foldr string-append "" (map separar-filas
+                                                        (map separar-columnas CONFIRMADOS-DPTO-HOY)))))
+
+(write-file "casos-por-dpto-antes.csv"
+            (string-append "Departamento, Casos Confirmados\n "
+                           (foldr string-append "" (map separar-filas
+                                                        (map separar-columnas CONFIRMADOS-DPTO-ANTES)))))
--- README.md
+++ README.md
@@ -2,30 +2,42 @@
 
 ## Exercises
 * [Homework 0]
-* [Homework 1]
+* [Homework 1] 
+    * [Homework 1 - Part 1]
+    * [Homework 1 - Part 2]
 * [Homework 2]
 * [Homework 3]
 * [Homework 4]
 * [Homework 5]
+    * [Homework 5 - Part 1]
+    * [Homework 5 - Part 2]
 * [Homework 6]
 * [Homework 7]
+    * [Homework 7 - Part 1]
+    * [Homework 7 - Part 2]
 
 ## Projects
 * [Project 1] - Receta para el diseño
 * [Project 2] - Programas interactivos
 * [Project 3] - Estructuras
-* [Project 4]
+* [Project 4] - Listas
 * [Project 5]
 * [Project 6]
 
 [Homework 0]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P0>
 [Homework 1]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P1>
+[Homework 1 - Part 1]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P1/P1.1>
+[Homework 1 - Part 2]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P1/P1.2>
 [Homework 2]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P2>
 [Homework 3]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P3>
 [Homework 4]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P4>
 [Homework 5]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P5>
+[Homework 5 - Part 1]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P5/P5.1>
+[Homework 5 - Part 2]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P5/P5.2>
 [Homework 6]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P6>
 [Homework 7]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P7>
+[Homework 7 - Part 1]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P7/P7.1>
+[Homework 7 - Part 2]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Exercises/P7/P7.2>
 
 [Project 1]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Projects/TP1>
 [Project 2]: <https://github.com/FabrizioMettini/Programming-I/tree/master/Projects/TP2>
